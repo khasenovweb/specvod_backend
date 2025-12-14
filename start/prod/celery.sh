215a74f961b965d51457c4a5a6d391f912a9d04e
khasenovweb <hasenovkirill93@gmail.com>
2025-12-14 15:47:08 +0500

    mac

--- start/prod/celery.sh
+++ start/prod/celery.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd "$SCRIPT_DIR/../../app"
-uv run celery -A app worker -l info
+/root/.local/bin/uv run celery -A app worker -l info
