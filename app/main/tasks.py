f9a0ca96262de6b71767aed23a32ff9bea505230
khasenovweb <hasenovkirill93@gmail.com>
2025-12-14 14:40:41 +0500

    mac

--- app/main/tasks.py
+++ app/main/tasks.py
@@ -13,6 +13,7 @@ from django.core import management
 @shared_task()
 def form_consult_email_send(data):
     form_name = data.get('form_name')
+    page_url = data.get('page_url')
     phone = data.get('phone') 
     name = data.get('name') 
 
@@ -20,12 +21,15 @@ def form_consult_email_send(data):
 
     if form_name:
         email_body += f"Форма: {form_name}\n"
+
+    if name:
+        email_body += f"Имя: {name}\n" 
     
     if phone:
         email_body += f"Телефон: {phone}\n" 
 
-    if name:
-        email_body += f"Имя: {name}\n" 
+    if page_url:
+        email_body += f"Страница: {page_url}\n" 
 
     send_email("Заявка с сайта", email_body, [])
     return "Отправка заявки с сайта"
